ce947a51c843d2b5b29fd7cc907dd88df2cd3b14
GiovaniSousaSousa <giovanisousa@outlook.com.br>
2023-07-05 23:58:44 -0300

    Retirando atributo nao utilizado

--- AplicativoDeNotas/src/Notebook.java
+++ AplicativoDeNotas/src/Notebook.java
@@ -1,20 +1,10 @@
-import java.util.List;
-
 public class Notebook {
     private final String path = "notebooks/";
-    private List<Note> notes;
 
     public String getPath() {
         return path;
     }
 
-    public List<Note> getNotes() {
-        return notes;
-    }
-
-    public void setNotes(List<Note> notes) {
-        this.notes = notes;
-    }
     
     
 }
