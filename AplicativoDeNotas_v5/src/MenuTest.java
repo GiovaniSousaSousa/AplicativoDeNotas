4e804c40ab4247b10a9993093cf73443b24b0b08
LeMartini <111762149+LeMartini@users.noreply.github.com>
2023-07-07 22:00:17 -0300

    Add files via upload

--- AplicativoDeNotas_v5/src/MenuTest.java
+++ AplicativoDeNotas_v5/src/MenuTest.java
@@ -8,6 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
 
+    /*Teste do menu,
+    Neste teste é verificado se o parâmetro, no caso abaixo seria o 6 ,
+     está na lista que foram inseridas as opções de seleção do menu, em caso positivo retorna True e valida teste*/
     @Test
     void menu() {
         List<String> validOptions = Arrays.asList("1", "2", "3", "4","5","6");
--- AplicativoDeNotas_v5/src/UserTest.java
+++ AplicativoDeNotas_v5/src/UserTest.java
@@ -10,27 +10,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    /*Teste do método de criação de notas,
+    nele é criado um arquivo e após verificado se o arquivo pode ser localizado dentro do diretório "Notebooks",
+     em caso positivo, retorna True, validando o teste.*/
     @org.junit.jupiter.api.Test
     public void testCreateNote() throws IOException {
-        String fileName = "Desenvolver1";
+        String fileName = "Desenvolver5";
         File directory = new File(new Notebook().getPath());
         File file = new File(directory, fileName);
         Assertions.assertTrue(file.createNewFile(), "arquivo não criado");
     }
 
+    /*Teste do método de visualização de notas,
+    Nele é verificado se é possível localizar o documento referenciado, em caso positivo retorna True e valida teste.*/
     @org.junit.jupiter.api.Test
     void getNote() throws IOException {
-        File arquivo = new File("notebooks/Desenvolver1");
-        Assertions.assertTrue(arquivo.exists(),"Arquivo Não Deletado");
+        File arquivo = new File("notebooks/Desenvolver2");
+        Assertions.assertTrue(arquivo.exists(),"Arquivo Localizado");
         }
 
+        /*Teste do método de delete de notas,
+        Nele é realizada a exclusão do arquivo referenciado e após verificado se está sendo localizado dentro do diretorio "Notebooks",
+        Caso não seja localizado, retorna False e valida teste.*/
     @Test
     void testdeleteNote() {
-       File arquivo = new File("notebooks/Desenvolver1");
+       File arquivo = new File("notebooks/Desenvolver3");
        arquivo.delete();
        Assertions.assertFalse(arquivo.exists(),"Arquivo Não Deletado");
     }
 
+    /*Teste do método de atualização de notas,
+     Em primeiro momento é recuperada informação que consta na nota, após atualizada informação
+     e por fim validado se a informação da nota anterior é igual a atual, em caso falso, valida teste.*/
     @org.junit.jupiter.api.Test
     void updateNote() throws IOException {
         String filePath = "notebooks/teste";
@@ -45,6 +56,10 @@ class UserTest {
         Assertions.assertNotEquals(fileContent,fileContent2);
     }
 
+    /*Teste de apresentação de arquivos em lista,
+    Primeiramente é recuperado o nome dos arquivos que estão dentro do diretorio,
+    após recuperar os arquivos eles são inseridos dentro de um array e por fim verificado se o array está nulo,
+    retorna False e valida teste. */
     @org.junit.jupiter.api.Test
     void listNotes() {
         String filePath = "notebooks/";
